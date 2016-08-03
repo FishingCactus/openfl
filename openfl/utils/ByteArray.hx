7484bf2202da37111041941ef1ed074470657065
Ramses Ladlani <ramses.ladlani@fishingcactus.com>
2016-08-03 14:57:14 +0200

    Fixed length reported by ByteArrayData to be the length of effective data and not the size of the working buffer

--- openfl/utils/ByteArray.hx
+++ openfl/utils/ByteArray.hx
@@ -188,10 +188,8 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 		
 		#if display
 		return 0;
-		#elseif flash
-		return this.length;
 		#else
-		return this.__length;
+		return this.length;
 		#end
 		
 	}
@@ -209,7 +207,6 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 			
 		}
 		
-		this.__length = value;
 		#end
 		
 		return value;
@@ -234,7 +231,7 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 	public var position:Int;
 	
 	private var __endian:Endian;
-	private var __length:Int;
+	private var __reserved:Int;
 	
 	
 	public function new (length:Int = 0) {
@@ -247,7 +244,7 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 		super (length, bytes.b);
 		#end
 		
-		__length = length;
+		__reserved = length;
 		endian = BIG_ENDIAN;
 		position = 0;
 		
@@ -256,7 +253,7 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 	
 	public function clear ():Void {
 		
-		__length = 0;
+		length = 0;
 		position = 0;
 		
 	}
@@ -296,8 +293,7 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 		
 		#end
 		
-		__length = this.length;
-		position = __length;
+		position = length;
 		
 	}
 	
@@ -360,15 +356,15 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 	
 	public function readBytes (bytes:ByteArray, offset:Int = 0, length:Int = 0):Void {
 		
-		if (length == 0) length = __length - position;
+		if (length == 0) length = this.length - position;
 		
-		if (position + length > __length) {
+		if (position + length > this.length) {
 			
 			throw new EOFError ();
 			
 		}
 		
-		if ((bytes:ByteArrayData).__length < offset + length) {
+		if ((bytes:ByteArrayData).__reserved < offset + length) {
 			
 			(bytes:ByteArrayData).__resize (offset + length);
 			
@@ -382,7 +378,7 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 	
 	public function readDouble ():Float {
 		
-		if (position + 8 > __length) {
+		if (position + 8 > length) {
 			
 			throw new EOFError ();
 			
@@ -396,7 +392,7 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 	
 	public function readFloat ():Float {
 		
-		if (position + 4 > __length) {
+		if (position + 4 > length) {
 			
 			throw new EOFError ();
 			
@@ -467,7 +463,7 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 	
 	public function readUnsignedByte ():Int {
 		
-		if (position < __length) {
+		if (position < length) {
 			
 			return get (position++);
 			
@@ -529,7 +525,7 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 	
 	public function readUTFBytes (length:Int):String {
 		
-		if (position + length > __length) {
+		if (position + length > this.length) {
 			
 			throw new EOFError ();
 			
@@ -581,7 +577,6 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 		
 		#end
 		
-		__length = this.length;
 		position = 0;
 		
 	}
@@ -710,24 +705,24 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 	private function __fromBytes (bytes:Bytes):Void {
 		
 		__setData (bytes);
-		__length = bytes.length;
 		
 	}
 	
 	
 	private function __resize (size:Int) {
 		
-		if (size > this.length) {
+		if (size > __reserved) {
 			
 			var bytes = Bytes.alloc (((size + 1) * 3) >> 1);
 			bytes.blit (0, this, 0, this.length);
 			__setData (bytes);
+			length = size;
 			
 		}
 		
-		if (__length < size) {
+		if (length < size) {
 			
-			__length = size;
+			length = size;
 			
 		}
 		
@@ -738,6 +733,7 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 		
 		b = bytes.b;
 		length = bytes.length;
+		__reserved = bytes.length;
 		
 		#if js
 		data = bytes.data;
@@ -755,7 +751,7 @@ abstract ByteArray(ByteArrayData) from ByteArrayData to ByteArrayData {
 	
 	@:noCompletion private inline function get_bytesAvailable ():Int {
 		
-		return __length - position;
+		return length - position;
 		
 	}
 	
