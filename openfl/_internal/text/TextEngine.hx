70a8792eb79a0edc13f4ad1c4eea076574341a53
Gauthier Billot <gauthier.billot@fishingcactus.com>
2018-05-15 09:49:13 +0200

    Fix lineLayoutGroups count vs lineIndex

--- openfl/_internal/text/TextEngine.hx
+++ openfl/_internal/text/TextEngine.hx
@@ -774,7 +774,7 @@ class TextEngine {
 			advances = null;
 			layoutGroups.push (layoutGroup);
 
-			if ( lineLayoutGroups.length == lineIndex ) {
+			while ( lineLayoutGroups.length <= lineIndex ) {
 				lineLayoutGroups.push([]);
 			}
 
