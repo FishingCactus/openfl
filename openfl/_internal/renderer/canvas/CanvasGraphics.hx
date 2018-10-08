36e6a272a57de9cde77f7038fdb9e3ccabab6a4d
Denis Franze <denis.franze@gameduell.de>
2018-10-08 15:32:25 +0200

    FLASH-2322 added bitmap null check //patch

--- openfl/_internal/renderer/canvas/CanvasGraphics.hx
+++ openfl/_internal/renderer/canvas/CanvasGraphics.hx
@@ -952,7 +952,7 @@ class CanvasGraphics {
 		context.save ();
 		context.transform (c.matrix.a, c.matrix.b, c.matrix.c, c.matrix.d, c.matrix.tx, c.matrix.ty);
 
-		if (!hitTesting) {
+		if (c.bitmap != null && !hitTesting) {
 
 			context.drawImage (c.bitmap.image.src, 0.0, 0.0, 1.0, 1.0);
 
